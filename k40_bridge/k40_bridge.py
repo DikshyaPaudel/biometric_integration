2953fd490bbac9ed7298f8285cea3b6af8a7b77c
DikshyaPaudel <dikshyapaudel9@gmail.com>
2026-02-19 09:41:55 +0545

    Update in logic of holiday

--- k40_bridge/k40_bridge.py
+++ k40_bridge/k40_bridge.py
@@ -119,7 +119,7 @@ def main():
     logger.info("="*50)
 
     # Get attendance from K40
-    # attendances = get_attendance_from_k40()
+    attendances = get_attendance_from_k40()
 
     # Filter to target date only
     attendances = [att for att in attendances if att.timestamp.date() == target_date]
