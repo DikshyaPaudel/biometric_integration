def01e2ae041f885b1bc61ba848c1295a84e7f2d
DikshyaPaudel <dikshyapaudel9@gmail.com>
2026-03-01 14:50:34 +0545

    sandbox avinash biometric configuration on k40_bridge with requiremnext.txt

--- k40_bridge/k40_bridge.py
+++ k40_bridge/k40_bridge.py
@@ -18,9 +18,8 @@ import requests
 # ============================================
 K40_IP = '192.168.18.200'
 K40_SERIAL = 'A6F521360285'
-# K40_SERIAL = 'A6F5215360564' # raindrop
 
-ERPNEXT_URL = 'https://demo-sb.raindropinc.com'
+ERPNEXT_URL = 'https://sandboxavinas-demo.raindropinc.com'
 WEBHOOK_PATH = '/api/method/biometric_integration.biometric_integration.biometric_integration.zkteco_push_attendance'
 
 # Log file — same folder as the script/exe
